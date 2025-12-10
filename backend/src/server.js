e86b9d86ad10bfdb9770bf26d5ac25c4ee5c5b67
Mickaa06 <mickarab36@gmail.com>
2025-12-10 10:36:55 +0300

    ajout d'un port d'ecoute sur server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -69,6 +69,6 @@ app.get('/health', (req, res) => {
   res.json({ status: 'OK', service: 'todo-backend' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Backend server running on port ${PORT}`);
 });
